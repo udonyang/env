d803d1ec79470bc4cc1827f58eff8858a4dcc585
Dwylkz <sgeteternal@126.com>
2013-07-28 15:22:43 +0800

    cppmain

--- cppmain/main.cc
+++ cppmain/main.cc
@@ -1,54 +1,13 @@
-//coder: SXDwylkz
-#if 1006100106
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
-#include <cctype>
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <stack>
-#include <queue>
-#include <string>
-#include <cstring>
-#include <cmath>
-//namespaces
 using namespace std;
-//constants
-const char input[] = "input.in",
-	  output[] = "output.out";
-const int inf32 = 0x7fffffff,
-	  maxn = 5e4 + 10,
-	  oo = -1;
-const long long inf64 = 0x7fffffffffffffffLL; 
+const int maxn = 5e4 + 10;
 
-//type definition
-typedef unsigned char UC;
-typedef unsigned long long ULL;
-typedef long long LL;
-typedef int int_a[maxn];
-typedef bool bool_a[maxn];
-typedef char char_a[maxn];
-typedef pair<int, int> II;
-
-//macro
-#define WHERE	puts(__func__);
-#define PA(H, I, S, E, F,...) {\
-	puts(H);\
-	for (int I = (S); I < (E); I++) {\
-		printf(F,##__VA_ARGS__);\
-	}\
-	puts("");\
-}
-
-#endif
-
-//main
 int main()
 {
 #if 1
 	freopen(input, "r", stdin);
-	//freopen(output, "w", stdout);
 #endif
 	for ( ; ; ) {
 	}
