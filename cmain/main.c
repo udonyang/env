e39dc002046ec55bca5a314c785acd1aed5628f7
Dwylkz <sgeteternal@126.com>
2013-07-28 15:17:55 +0800

    hoho

---
+++ cmain/main.c
@@ -0,0 +1,14 @@
+#pragma comment(linker, "\STACK:65535000")
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+#if 1
+	freopen(input, "r", stdin);
+#endif
+	for ( ; ; ) {
+	}
+	return 0;
+}
