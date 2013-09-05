93390c485717ec4bb09c1e732402b6ded58021ec
Dwylkz <sgeteternal@126.com>
2013-09-05 11:08:43 +0800

    Thu Sep  5 11:08:43 CST 2013

--- gitpush.sh
+++ gitpush.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 git add .
-git commit -m "$(date)"
+git commit -a -m "$(date)"
 git push -v
 cd ..
 cd ACMPS
 git add .
-git commit -m "$(date)"
+git commit -a -m "$(date)"
 git push -v
 exit
