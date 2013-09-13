b4d2d0f06bcea6df7dbe1086ac402576f9b113ad
Dwylkz <sgeteternal@126.com>
2013-09-13 22:53:37 +0800

    Fri Sep 13 22:53:37 CST 2013

--- func_style/graph/main.cc
+++ func_style/graph/main.cc
@@ -4,21 +4,19 @@
 using namespace std;
 const int N = 10;
 
-namespace graph {
-  struct edge_t {
-    int v, to;
-  };
-  vector<edge_t> E;
-  int L[N];
-  void init() {
-    E.clear();
-    memset(L, -1, sizeof(L));
-  }
-  void add(int u, int v) {
-    edge_t t = {v, L[u]};
-    L[u] = E.size();
-    E.push_back(t);
-  }
+struct edge_t {
+  int v, to;
+};
+vector<edge_t> E;
+int L[N];
+void init() {
+  E.clear();
+  memset(L, -1, sizeof(L));
+}
+void add(int u, int v) {
+  edge_t t = {v, L[u]};
+  L[u] = E.size();
+  E.push_back(t);
 }
 
 //main
