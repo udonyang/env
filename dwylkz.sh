f23a229d9da528563583f59e44314384154c7232
Dwylkz <xellosgeteternal@gmail.com>
2014-02-19 22:25:28 +0800

    Wed Feb 19 22:25:28 CST 2014

--- dwylkz.sh
+++ dwylkz.sh
@@ -7,6 +7,3 @@ export CLASSPATH=.:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar
 export TEXHOME=/usr/local/texlive/2013
 export MANPATH=$MANPATH:$TEXHOME/texmf-dist/doc/man
 export PATH=$PATH:$TEXHOME/bin/i386-linux
-
-setxkbmap -option caps:swapescape
-xbindkeys -f $DHOME/.xbindkeysrc
