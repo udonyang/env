3505a7d06c5a075df018e4382be7d52d65080e45
Dwylkz <xellosgeteternal@gmail.com>
2014-05-01 15:22:13 +0800

    Thu May  1 15:22:13 HKT 2014

---
+++ language/c and c++/play/play.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void Lambda();
+void Strtok();
+void Stream();
+void Signal();
--- language/c and c++/pthread/6. Manager and Worker/RunnableInterface.h
+++
@@ -1,9 +0,0 @@
-#ifndef DAC_RUNNABLE_INTERFACE_H
-#define DAC_RUNNABLE_INTERFACE_H
-
-typedef struct {
-	int (*run)(void*);
-	int (*drop)(void*);
-} RunnableInterface;
-
-#endif
